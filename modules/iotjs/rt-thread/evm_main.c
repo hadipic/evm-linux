f1a8deb0c4271b1d9982afa9e431896e85dc02a7
zhe wang <707777212@qq.com>
2022-08-27 20:09:17 +0800

    rt-thread增加repl模式

--- modules/iotjs/rt-thread/evm_main.c
+++ modules/iotjs/rt-thread/evm_main.c
@@ -3,7 +3,7 @@
 
 void *evm_malloc(size_t size)
 {
-    void * m = malloc(size);
+    void * m = rt_malloc(size);
     if( m ) 
         memset(m, 0 ,size);
     return m;
@@ -12,12 +12,30 @@ void *evm_malloc(size_t size)
 void evm_free(void * p)
 {
     if(p) 
-        free(p);
+        rt_free (p);
+}
+
+void *evm_realloc(void *p, size_t size)
+{
+    void * m = rt_realloc(p, size);
+    if( m ) 
+        memset(m, 0 ,size);
+    return m;
 }
 
 void evm_main (void) {
     evm_t *env = evm_init();
     evm_module_init(env);
-    evm_val_t res = evm_run_file(env, "main.js");
-    evm_val_free(env, res);
+#ifdef CONFIG_EVM_MODULE_REPL
+    evm_run_repl(env);
+    printf("quit repl mode\r\n");
+#else
+    evm_run_file(env, "main.js");
+#endif
+
+    while(1){
+    #ifdef CONFIG_EVM_MODULE_PROCESS
+        evm_module_process_poll(env);
+    #endif
+    }
 }
---
+++ modules/iotjs/rt-thread/rtt_module_repl.c
@@ -0,0 +1,32 @@
+#ifdef CONFIG_EVM_MODULE_REPL
+#include "evm_module.h"
+#include <rtthread.h>
+
+static rt_device_t fd_console;
+
+void evm_repl_init(evm_t *e) {
+    fd_console = rt_device_find("uart0");
+    rt_device_open(fd_console, RT_DEVICE_FLAG_STREAM);
+}
+
+char evm_repl_tty_read(evm_t *e)
+{
+    EVM_UNUSED(e);
+    char buffer;
+    if( rt_device_read(fd_console, 0, &buffer, 1) == 1) 
+        return buffer;
+    else {
+        rt_thread_mdelay(10);
+        return 0;
+    }
+}
+
+void evm_repl_tty_write(int n, char *s)
+{
+    rt_device_write(fd_console, 0, s, n);
+}
+
+
+#endif
+
+
