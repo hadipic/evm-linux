93e82b0444e16633b667286d35c9d7032877eab9
zhe wang <707777212@qq.com>
2022-11-13 16:37:28 +0800

    run pikascript bytecode

--- engines/pikascript/evm.c
+++ engines/pikascript/evm.c
@@ -123,7 +123,22 @@ void evm_deinit(evm_t *e) {
 }
 
 void evm_run_file(evm_t *e, const char *path) {
-    pikaVM_runFile(e, (char*)path);
+    size_t len = strlen(path);
+    if(path[len - 2] == '.' && path[len - 1] == 'o') {
+        Args buffs = {0};
+        Arg* file_arg = arg_loadFile(NULL, path);
+        pika_assert(NULL != file_arg);
+        if (NULL == file_arg) {
+            return;
+        }
+        uint8_t* lines = (uint8_t*)arg_getBytes(file_arg);
+        /* clear the void line */
+        pikaVM_runByteCode(e, lines);
+        arg_deinit(file_arg);
+        strsDeinit(&buffs);
+    } else {
+        pikaVM_runFile(e, (char*)path);
+    }
 }
 
 evm_val_t evm_run_string(evm_t *e, const char *source) {
