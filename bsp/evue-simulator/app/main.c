16b2780c28441a8cce0b42e43387057a003f8762
zhe wang <707777212@qq.com>
2022-11-30 12:31:53 +0800

    lvgl style

--- bsp/evue-simulator/app/main.c
+++ bsp/evue-simulator/app/main.c
@@ -107,6 +107,7 @@ int main(int argc, char** argv) {
         /* Periodically call the lv_task handler.
          * It could be done in a timer interrupt or an OS task too.*/
         lv_timer_handler();
+        lv_task_handler();
         usleep(5 * 1000);
     }
 
